23cf3853d305a3946bcd01cdae05977c76eb3eed
Andre <dmastergame74@gmail.com>
2026-08-22 18:27:56 -0300

    set 'trust proxy' to 1

--- src/app.js
+++ src/app.js
@@ -9,6 +9,7 @@ import { errorHandler } from './middleware/errorHandler.js';
 const app = express();
 
 app.disable('x-powered-by');
+app.set('trust proxy', 1);
 app.use(helmet());
 app.use(cors({ origin: config.corsOrigin }));
 app.use(express.json({ limit: '10kb' }));
--- src/server.js
+++ src/server.js
@@ -2,5 +2,5 @@ import app from './app.js';
 import { config } from './config.js';
 
 app.listen(config.port, () => {
-  console.log(`API rodando em http://localhost:${config.port} (${config.nodeEnv})`);
+  console.log(`API rodando na porta ${config.port}: (${config.nodeEnv})`);
 });
